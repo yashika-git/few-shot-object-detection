9316ad49403b0a248c4e063c36fd8946e4d99e11
Yashika Jain <40869641+yashika-git@users.noreply.github.com>
2022-09-21 16:56:33 +0530

    Update builtin.py

--- fsdet/data/builtin.py
+++ fsdet/data/builtin.py
@@ -186,7 +186,7 @@ def register_all_lvis(root="datasets"):
 
 
 # ==== Predefined splits for PASCAL VOC ===========
-def register_all_pascal_voc(root="datasets"):
+def register_all_pascal_voc(root="../input/dataset"):
     # SPLITS = [
     #     ("voc_2007_trainval", "VOC2007", "trainval"),
     #     ("voc_2007_train", "VOC2007", "train"),
